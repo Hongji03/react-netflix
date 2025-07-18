08a624581f98d3816847a9002e3044a52085c4ac
Jihyun <hjh20030615@gmail.com>
2025-07-18 22:41:52 +0900

    안쓰는 코드 정리

--- src/index.js
+++ src/index.js
@@ -3,17 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ThemeProvider } from 'styled-components';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const dummyTheme = {};
 
 root.render(
-  <BrowserRouter>
-    {/* <ThemeProvider theme={dummyTheme}> */}
-      <App />
-    {/* </ThemeProvider> */}
+  <BrowserRouter basename='react-netflix'>
+    <App />
   </BrowserRouter>
 );
 
